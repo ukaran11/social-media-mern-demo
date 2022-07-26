41f8cca11831c77f13a2fc530bd82fadc5ac25f9
unknown <ukaran11@gmail.com>
2022-07-27 03:03:50 +0530

    Utsav | Added Register route and adding user

--- node-rest-api/routes/auth.js
+++ node-rest-api/routes/auth.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-  res.send("auth route")
-});
+// Register
+router.post('/register',async (req, res) => {
+  const newUser =await new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch(err) {
+    console.log(err);
+  }
+})
+
 
 module.exports = router;
