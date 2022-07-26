4efc2429935f68a169c87286294b22abdd50cce4
unknown <ukaran11@gmail.com>
2022-07-27 03:29:45 +0530

    Utsav | Introduced password hashing using bcrypt

--- node-rest-api/routes/auth.js
+++ node-rest-api/routes/auth.js
@@ -1,15 +1,23 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
 const User = require('../models/User');
 
 // Register
 router.post('/register',async (req, res) => {
-  const newUser =await new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
   try {
+    // Generate hash of password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create new user
+    const newUser =await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // Save user and return response
     const user = await newUser.save();
     res.status(200).json(user);
   } catch(err) {
