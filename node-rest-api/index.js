0261d60a50382693d1125f3b6b512a766083951d
unknown <ukaran11@gmail.com>
2022-07-27 22:16:54 +0530

    Utsav | Added Post Router

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -6,6 +6,8 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+
 dotenv.config();
 
 // Middleware
@@ -17,8 +19,11 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
   console.log('Connected to Mongo');
 });
 
+// Routes
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute)
+app.use('/api/posts', postRoute)
+
 app.listen(8800, () => {
   console.log('Backend server is running')
 })
