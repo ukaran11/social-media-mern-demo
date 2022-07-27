7872d808b09f10d17d0b6e050ebc2614e58a4fc3
unknown <ukaran11@gmail.com>
2022-07-27 23:15:48 +0530

    Utsav | Added Post Create

--- node-rest-api/routes/posts.js
+++ node-rest-api/routes/posts.js
@@ -1,4 +1,21 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Post = require("../models/Post");
 
+// Create a Post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
 
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update a Post
+// Delete a Post
+// Like a Post
+// Get a Post
+// Get timeline Posts
 module.exports = router;
