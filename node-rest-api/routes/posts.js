70eb8f70eaf22c0c7a820988771800e907cd0137
unknown <ukaran11@gmail.com>
2022-07-28 04:06:56 +0530

    Utsav | Added timeline api

--- node-rest-api/routes/posts.js
+++ node-rest-api/routes/posts.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
-
+const User = require("../models/User");
 // Create a Post
 router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
@@ -60,14 +60,30 @@ router.put("/:id/like", async (req, res) => {
 });
 
 // Get a Post
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     res.status(200).json(post);
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
 
 // Get timeline Posts
+router.get("/timeline/all", async (req, res) => {
+  let postArray = [];
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
