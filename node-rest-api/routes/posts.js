34e937fed724c3f783569f3d698e157bea6d6590
unknown <ukaran11@gmail.com>
2022-07-28 03:07:38 +0530

    Utsav | Added Like/Dislike api

--- node-rest-api/routes/posts.js
+++ node-rest-api/routes/posts.js
@@ -14,35 +14,51 @@ router.post("/", async (req, res) => {
 });
 
 // Update a Post
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    if(post.userId === req.body.userId) {
-      await post.updateOne({$set: req.body});
-      res.status(200).json('The post has been updated');
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("The post has been updated");
     } else {
-      res.status(403).json('You can only update your own posts')
+      res.status(403).json("You can only update your own posts");
     }
   } catch (err) {
     res.status(404).json(err);
   }
-})
+});
 
 // Delete a Post
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    if(post.userId === req.body.userId) {
+    if (post.userId === req.body.userId) {
       await post.deleteOne();
-      res.status(200).json('The post has been deleted');
+      res.status(200).json("The post has been deleted");
     } else {
-      res.status(403).json('You can only delete your own posts')
+      res.status(403).json("You can only delete your own posts");
     }
   } catch (err) {
     res.status(404).json(err);
   }
-})
-// Like a Post
+});
+
+// Like/Dislike a Post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Get a Post
 // Get timeline Posts
 module.exports = router;
