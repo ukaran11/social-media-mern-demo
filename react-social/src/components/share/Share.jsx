bed2195047263de56f2ec7c959f7ad0cc0dff83e
unknown <ukaran11@gmail.com>
2022-07-30 18:52:32 +0530

    Utsav | Added shareoptions

--- react-social/src/components/share/Share.jsx
+++ react-social/src/components/share/Share.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './share.css';
-import { PermMedia } from '@material-ui/icons';
+import { PermMedia, Label, Room, EmojiEmotions } from '@material-ui/icons';
 
 const Share = () => {
   return (
@@ -14,8 +14,20 @@ const Share = () => {
         <div className="shareBottom">
           <div className="shareOptions">
             <div className="shareOption">
-              <PermMedia className='shareIcon' />
+              <PermMedia htmlColor='tomato' className='shareIcon' />
               <span className="shareOptionText">Photo or Video</span>
+            </div>
+            <div className="shareOption">
+              <Label htmlColor='blue' className='shareIcon' />
+              <span className="shareOptionText">Tag</span>
+            </div>
+            <div className="shareOption">
+              <Room htmlColor='green' className='shareIcon' />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <EmojiEmotions htmlColor='goldenrod' className='shareIcon' />
+              <span className="shareOptionText">Feelings</span>
             </div>
           </div>
         </div>
