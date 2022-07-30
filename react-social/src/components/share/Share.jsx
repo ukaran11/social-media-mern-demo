bb1852b96f19bab242b6d06e9940ef504a8a35a5
unknown <ukaran11@gmail.com>
2022-07-30 19:25:38 +0530

    Utsav | Worked on share comp

--- react-social/src/components/share/Share.jsx
+++ react-social/src/components/share/Share.jsx
@@ -30,6 +30,7 @@ const Share = () => {
               <span className="shareOptionText">Feelings</span>
             </div>
           </div>
+          <button className='shareButton'>Share</button>
         </div>
       </div>
     </div>
