3b3407a88c828e8cf66dc14ac10555955f61dbcf
unknown <ukaran11@gmail.com>
2022-07-31 18:05:41 +0530

    Utsav | Injected dummy users dynamically to Post comp

--- react-social/src/components/post/Post.jsx
+++ react-social/src/components/post/Post.jsx
@@ -1,20 +1,22 @@
-import { MoreVert } from '@material-ui/icons';
-import './post.css';
+import { MoreVert } from "@material-ui/icons";
+import "./post.css";
+import { Users } from "../../DummyData";
 
+const Post = ({ post }) => {
 
-const Post = ({post}) => {
-  console.log(post);
   return (
-    <div className='post'>
+    <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img className="postProfileImg" src="/assets/person/1.jpeg" />
-            <span className="postUsername">Utsav Karan</span>
+            <img className="postProfileImg" src={Users.filter((u) => u.id === post.userId)[0].profilePicture} />
+            <span className="postUsername">
+              {Users.filter((u) => u.id === post.userId)[0].username}
+            </span>
             <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
-            <MoreVert/>
+            <MoreVert />
           </div>
         </div>
 
@@ -25,8 +27,8 @@ const Post = ({post}) => {
 
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon"  src="assets/like.png" alt="" />
-            <img className="likeIcon"  src="assets/heart.png" alt="" />
+            <img className="likeIcon" src="assets/like.png" alt="" />
+            <img className="likeIcon" src="assets/heart.png" alt="" />
             <span className="postLikeCounter">{post.like} people liked it</span>
           </div>
           <div className="postBottomRight">
@@ -35,7 +37,7 @@ const Post = ({post}) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
