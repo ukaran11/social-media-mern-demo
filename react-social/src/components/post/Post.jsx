cc63f4a69101881975ce04a523e09e5cffe7580f
unknown <ukaran11@gmail.com>
2022-07-31 17:42:18 +0530

    Utsav | Injected dummy posts dynamically to Post comp

--- react-social/src/components/post/Post.jsx
+++ react-social/src/components/post/Post.jsx
@@ -2,7 +2,8 @@ import { MoreVert } from '@material-ui/icons';
 import './post.css';
 
 
-const Post = () => {
+const Post = ({post}) => {
+  console.log(post);
   return (
     <div className='post'>
       <div className="postWrapper">
@@ -10,7 +11,7 @@ const Post = () => {
           <div className="postTopLeft">
             <img className="postProfileImg" src="/assets/person/1.jpeg" />
             <span className="postUsername">Utsav Karan</span>
-            <span className="postDate">5 mins ago</span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert/>
@@ -18,18 +19,18 @@ const Post = () => {
         </div>
 
         <div className="postCenter">
-          <span className="postText">Hey! Its my first post!</span>
-          <img className="postImg" src="assets/post/1.jpeg" alt="" />
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={post.photo} alt="" />
         </div>
 
         <div className="postBottom">
           <div className="postBottomLeft">
             <img className="likeIcon"  src="assets/like.png" alt="" />
             <img className="likeIcon"  src="assets/heart.png" alt="" />
-            <span className="postLikeCounter">32 people liked it</span>
+            <span className="postLikeCounter">{post.like} people liked it</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">9 comments</span>
+            <span className="postCommentText">{post.comment} comments</span>
           </div>
         </div>
       </div>
