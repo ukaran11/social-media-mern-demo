4130713173680692ebc7df35ba0586696190954f
unknown <ukaran11@gmail.com>
2022-08-01 03:01:11 +0530

    Utsav | Shifted rightbar friends to online comp

--- react-social/src/components/rightbar/Rightbar.jsx
+++ react-social/src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,6 @@
 import "./rightbar.css";
-
+import Online  from '../online/Online';
+import { Users } from "../../DummyData";
 const Rightbar = () => {
   return (
     <div className="rightbar">
@@ -16,50 +17,9 @@ const Rightbar = () => {
         <img src="assets/ad.png" alt="" className="rightbarAd" />
         <h4 className="rightbarTitle">Online Friends</h4>
         <ul className="rightbarFriendList">
-          <li className="rightbarFriend">
-            <div className="rightbarProfileImgContainer">
-              <img
-                className="rightbarProfileImg"
-                src="assets/person/3.jpeg"
-                alt=""
-              />
-              <span className="rightbarOnline"></span>
-            </div>
-            <span className="rightbarUsername">John Carter</span>
-          </li>
-          <li className="rightbarFriend">
-            <div className="rightbarProfileImgContainer">
-              <img
-                className="rightbarProfileImg"
-                src="assets/person/3.jpeg"
-                alt=""
-              />
-              <span className="rightbarOnline"></span>
-            </div>
-            <span className="rightbarUsername">John Carter</span>
-          </li>
-          <li className="rightbarFriend">
-            <div className="rightbarProfileImgContainer">
-              <img
-                className="rightbarProfileImg"
-                src="assets/person/3.jpeg"
-                alt=""
-              />
-              <span className="rightbarOnline"></span>
-            </div>
-            <span className="rightbarUsername">John Carter</span>
-          </li>
-          <li className="rightbarFriend">
-            <div className="rightbarProfileImgContainer">
-              <img
-                className="rightbarProfileImg"
-                src="assets/person/3.jpeg"
-                alt=""
-              />
-              <span className="rightbarOnline"></span>
-            </div>
-            <span className="rightbarUsername">John Carter</span>
-          </li>
+          {Users.map(u => (
+            <Online key={u.id} user={u} />
+          ))}
         </ul>
       </div>
     </div>
